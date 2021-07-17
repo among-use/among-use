c95b096c4179c9c73191206a1e1f18761b0f4cde
suzuki-motomichi <cw.sm31@gmail.com>
2021-07-17 20:11:56 +0900

    Usersルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+
+  # Users
+  resources :users, only: %i[new create]
 end
