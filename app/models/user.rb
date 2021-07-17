5261fe5f06f61fda472ce43a1ae02cc0c7ce3018
suzuki-motomichi <cw.sm31@gmail.com>
2021-07-18 04:27:13 +0900

    Fix: Userモデル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,13 @@ class User < ApplicationRecord
   has_many :participants, dependent: :destroy
 # has_many :participant_events, through: :participants, source: :event
 
-  validates :name, presense: true
-  validates :email, uniquness: true ,presence: true, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i}
+  validates :name, presence: true
+  validates :email, uniqueness: true, presence: true, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i}
   validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-  validates :reset_password_token, uniquness: true, allow_nil: true
-
+  # validates :reset_password_token, uniqueness: true, allow_nil: true
+  # 必要であれば上記カラムを追加
   enum role: { general: 0, admin: 0 }
 end
