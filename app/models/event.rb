3f5b0e33d9302562e552b0c072784dc9c81bf82c
yunosuke924 <y.minegishi0924@gmail.com>
2021-07-17 22:58:51 +0900

    ADD:関連づけ設定の追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
-  # イベントのバリデーションは議論をしてから決めます。
+  belongs_to :user # このユーザーはイベントの作成者を意味します。
+  has_many :participants, dependent: :destroy # イベントは複数の参加者ユーザーと紐づいています。
+  has_many :participants_users, through: :participants, source: :user #特定のイベントの参加者情報を取得
 end
