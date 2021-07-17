4d60736637a59e869e8b19efec05ecbf8a5a781f
suzuki-motomichi <cw.sm31@gmail.com>
2021-07-17 21:45:04 +0900

    [ADD]: トップページとログイン画面のレイアウト #15

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'top#index'
+
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
