bdeca47d7a4eb0001f454c43422db8e3ca7f1d48
yunosuke924 <y.minegishi0924@gmail.com>
2021-07-18 14:08:28 +0900

    FIX:routesのコンフリ解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  resources :events,only: %i[index create destroy edit new update]
-=======
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   root 'top#index'
->>>>>>> master
 
+  resources :events,only: %i[index create destroy edit new update]
+  
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
