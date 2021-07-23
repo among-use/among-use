89681bfb1375ff3c9457ac26420b44b38c9354d5
yunosuke924 <y.minegishi0924@gmail.com>
2021-07-23 18:11:39 +0900

    ADD:Eventモデルバリデーションの設定

--- db/migrate/20210717040955_create_events.rb
+++ db/migrate/20210717040955_create_events.rb
@@ -1,11 +1,11 @@
 class CreateEvents < ActiveRecord::Migration[6.1]
   def change
     create_table :events do |t|
-      t.string :message
-      t.datetime :start_datetime
-      t.datetime :finish_datetime
-      t.string :rule
-      t.integer :max_crews
+      t.string :message, null: false
+      t.datetime :start_datetime, null: false
+      t.datetime :finish_datetime, null: false
+      t.integer :rule, null: false, default: 0
+      t.integer :max_crews, null: false
 
       t.timestamps
     end
