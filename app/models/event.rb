eb3d5f5054c46f13b33aead832d8a599f60f8dd4
yunosuke924 <y.minegishi0924@gmail.com>
2021-07-23 17:55:43 +0900

    ADD:Eventモデルにenum属性ルールを追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
   belongs_to :user # このユーザーはイベントの作成者を意味します。
   has_many :participants, dependent: :destroy # イベントは複数の参加者ユーザーと紐づいています。
   has_many :participants_users, through: :participants, source: :user #特定のイベントの参加者情報を取得
+
+  enum rule: { normal: 0, catch: 1, hide: 2 } #ルール属性、初期値はnoraml
 end
