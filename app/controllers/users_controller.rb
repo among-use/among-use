aea0026d4226f95a106eb1f05c57ecdc8bcf65a0
saitoizumi <ilrw3110123@gmail.com>
2021-07-28 00:57:53 +0900

    add: usersとprofilesコントローラー作成 #9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,31 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    @profile = user.build_profile(user_params)
+    if @profile.update
+      redirect_to user_path(user)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 end
