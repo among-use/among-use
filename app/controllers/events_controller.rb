9ac49fbe406d8cdf52cc97e3e0680771863bfe6b
坂本航祐 <program8910@gmail.com>
2021-07-29 11:35:25 +0900

    Add イベント作成〜保存及びモーダルfade out実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,11 +3,25 @@ class EventsController < ApplicationController
   end
   
   def create
+    @event = Event.new(event_params)
+    @event.user_id = current_user.id
+    if @event.save
+      respond_to do |format|
+        format.html { redirect_to root_path } 
+        format.js
+      end  #create.js.erbを探してその中の処理を実行する
+    end
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+
+  def event_params
+    params.permit(:start_datetime, :finish_datetime, :message, :max_crews).merge(rule: params[:rule].to_i)
   end
 end
