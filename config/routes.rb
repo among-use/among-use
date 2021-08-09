92331148fe6f5edc24b9967ce6104ea6b43aee08
yunosuke924 <y.minegishi0924@gmail.com>
2021-08-09 21:39:24 +0900

    ADD:利用規約、ポリシーページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,18 @@
 Rails.application.routes.draw do
 
-  resources :participants,  only: [:index, :create, :destroy]
-
   root 'top#index'
 
+  resources :users  
   resources :events,only: %i[index create destroy edit new update]
+  resources :participants,  only: [:index, :create, :destroy]
+  resources :profiles, only: %i[new]
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
-
-  resources :profiles, only: %i[new]
-
   post "oauth/callback", to: "oauths#callback"
   get "oauth/callback", to: "oauths#callback"
   get "oauth/:provider", to: "oauths#oauth", as: :auth_at_provider
-
-  # Users
-  resources :users
+  get "top/terms", to: "top#terms"
+  get "top/privacy", to: "top#privacy"
 end
