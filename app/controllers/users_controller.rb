a88bbf2ec0fd328a201832965d7fc074b06308d5
sakita daiki <kurukuruskt28@gmail.com>
2021-08-15 04:51:36 +0900

    update: 新規登録後にログインするように実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path # 後でリダイレクト先を変更
+      auto_login(@user)
+      redirect_to root_path
     else
       render :new
     end
@@ -36,7 +37,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
   def user_set
