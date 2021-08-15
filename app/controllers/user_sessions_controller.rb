166293f0119a553d5ef8959a33db9ce42cd8a948
sakita daiki <kurukuruskt28@gmail.com>
2021-08-15 09:47:22 +0900

    add: フラッシュメッセージを追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,14 +5,17 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
+      flash[:success] = 'ログインしました。'
       redirect_to root_path
     else
+      flash.now[:danger] = 'ログインに失敗しました。'
       render :new
     end
   end
 
   def destroy
     logout
+    flash[:success] = 'ログアウトしました。'
     redirect_to root_path
   end
 end
